8ba08e533fb947acfc5c8ca73fb590b11fdae4c0
kundawu-cmd <54656648+kundawu-cmd@users.noreply.github.com>
2019-09-25 22:04:01 -0700

    Create SignOut.js

---
+++ client/src/components/SignOut.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const SignOut = () => {
+    return (
+        <u1 className = "right">
+            <li><NavLink to = '/'>Welcome</NavLink></li>
+        </u1>
+    )
+}
+
+export default SignOut
