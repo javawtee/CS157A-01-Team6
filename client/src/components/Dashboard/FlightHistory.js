a542bdc65e086e9b8f14cb3bf57d10ce9e21e259
kundawu-cmd <54656648+kundawu-cmd@users.noreply.github.com>
2019-09-25 22:03:44 -0700

    Create FlightHistory.js

---
+++ client/src/components/Dashboard/FlightHistory.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FlightHistory = () => {
+    return (
+        <div className = "Flight-List section">
+                <div className = "card-content grey-text text-darken-3">
+                    <span className = "Airline-title">Airline</span>
+                    <p>Served by AST 6, this is a list of travels in AST 6</p>
+                    <p className = "grey-text">Date: </p>
+                </div>
+            </div>
+    )
+}
+
+export default FlightHistory
