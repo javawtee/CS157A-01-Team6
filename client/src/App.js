6847a8fd3aa6928db4665b3ee84e9a4437b83736
kundawu-cmd <54656648+kundawu-cmd@users.noreply.github.com>
2019-09-25 22:03:24 -0700

    Update App.js
    Modified the app.js to take navbar and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import 'stylesheets/App.css';
-import {Switch, Route} from 'react-router-dom'
-
+import { Switch, Route } from 'react-router-dom'
 import Welcome from 'pages/Welcome'
 import P404 from 'pages/P404'
+import Dashboard from 'pages/Dashboard'
+import NavBar from 'components/NavBar'
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" render={() => <Welcome />} />
+        <Route exact path="/dashboard" render={() => <NavBar content={<Dashboard />} />} />
         <Route exact path="*" render={() => <P404 />} />
       </Switch>
     </div>
@@ -17,3 +20,4 @@ function App() {
 }
 
 export default App;
+
