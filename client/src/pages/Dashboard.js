7b45ec12f406d9bdc2b36959ff6b114c352eeb3b
kundawu-cmd <54656648+kundawu-cmd@users.noreply.github.com>
2019-09-27 09:24:10 -0700

    resolved conflicts

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,10 +5,10 @@ class Dashboard extends Component {
 
     render() {
         return (
-        <div className="uk-background-cover" style={{backgroundImage:"url(media/welcome-bg.png)"}} uk-height-viewport="expand: true">
+        <div className="uk-background-cover" uk-height-viewport="expand: true">
                 <div className="uk-width-1-2@s uk-align-center"></div>
                 <h1 style={{ color: "red", fontWeight: "bold" }}>Dashboard</h1>
-                <Notifications hello={'Dashboard'} />
+                <Notifications/>
             </div>
         )
     }
