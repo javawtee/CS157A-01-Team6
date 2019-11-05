30e12536d49b4b040a463974070ded1d6ee69b39
Tee <47378564+javawtee@users.noreply.github.com>
2019-11-04 21:56:33 -0800

    fix problem mysql configuration for each team member
    - Will add .gitignore to ignore change in mysql_config after this

--- server/api/connector.js
+++ server/api/connector.js
@@ -1,15 +1,5 @@
 const mysql = require('mysql')
 
-var connConfig = {
-    host: '127.0.0.1',
-    user: 'root', // update this to match your local mysql user
-    password: 'password', // update this to match your local mysql password for user
-    database: 'cs157a_project',
-    // connectionLimit: 20,
-    port: 3306,
-    strict: false,
-}
-
-const connection = mysql.createPool(connConfig)
+const connection = mysql.createPool(require('./configs/mysql_config'))
 
 module.exports = connection
