d3af04238f63b2977c2c0416508f7afb82077cfa
Tee <47378564+javawtee@users.noreply.github.com>
2019-12-07 23:43:02 -0800

    Last push
    - Final Report
    - SQL scripts for project database

--- Setup/mysql/complete_project_database_v2.sql
+++ Setup/mysql/complete_project_database_v2.sql
@@ -207,27 +207,6 @@ INSERT INTO `preference` VALUES (1,'any','any','any',200,'price'),(2,'12to6','an
 /*!40000 ALTER TABLE `preference` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `test_login`
---
-
-DROP TABLE IF EXISTS `test_login`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `test_login` (
-  `id` int(11) NOT NULL,
-  `user_id` varchar(45) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `user_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `test_login`
---
-
 LOCK TABLES `test_login` WRITE;
 /*!40000 ALTER TABLE `test_login` DISABLE KEYS */;
 INSERT INTO `test_login` VALUES (1,'javawtee@gmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92','Thong Le'),(2,'johnpatmcginley@gmail.com','472bbe83616e93d3c09a79103ae47d8f71e3d35a966d6e8b22f743218d04171d','John McGinley'),(3,'kunda.wu@sjsu.edu','b7158b64a98516b31d0c23609f69265a868c594dda5b3c8da9e13159e209c9b6','Kunda Wu');
